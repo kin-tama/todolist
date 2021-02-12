10d2d1e52c83d64cee4fac52a4c2a3fb279fb0cc
kin-tama <65250935+kin-tama@users.noreply.github.com>
2021-02-12 18:17:44 +0300

    draft version 2

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div>
-    <header v-show="!getLength">
-        <button>
-            add new task
-        </button>
-    </header>
     <main class="main">
-      <form class="main__form" action="#" method="post">
-        <input class="main__task main__task--name" type="text" placeholder="Insert your task name" maxlength="30" required
+      <form class="task" action="#" method="post">
+        <input class="task__description task__name" type="text" placeholder="Insert your task name" maxlength="30" required
           v-bind:value="newTaskName"
           v-on:input="newTaskName = $event.target.value"
-          v-on:keydown.enter.prevent="addNewTask()"
-          >
-        <span class="counter">
+          v-on:keydown.enter.prevent="addNewTask()">
+
+        <span class="task__counter">
             {{newTaskName.length}}/30
         </span>
 
-        <textarea class="main__task" name="task" id="task" placeholder="Describe your task for today"
-        v-bind:value="newTaskDescription"
-        v-on:input="newTaskDescription = $event.target.value"></textarea>
+        <textarea class="task__description" name="task" id="task" placeholder="Describe your task for today"
+          v-bind:value="newTaskDescription"
+          v-on:input="newTaskDescription = $event.target.value"></textarea>
 
-        <label class="priority-label" for="priority">Priority: </label>
+        <label class="task__priority-label" for="priority">Priority: </label>
 
-        <select class="choose_priority" name="priority" id="priority"
+        <select class="task__choose_priority" name="priority" id="priority"
           v-bind="newTaskPriority"
           v-on:change="newTaskPriority = $event.target.value">
 
@@ -33,41 +28,43 @@
         </select>
 
         <section class="buttons">
-            <button class="button button--sort" title="Sort from high priority to low" v-on:click.prevent="sortTaskList()" > Sort </button>
-            <button class="button button--add" title="Click to add your new task" v-on:click.prevent="addNewTask()"> Add </button>
+            <button class="buttons__button button button--sort" title="Sort from high priority to low" v-on:click.prevent="sortTaskList()" > Sort </button>
+            <button class="buttons__button button button--add" title="Click to add your new task" v-on:click.prevent="addNewTask()"> Add </button>
         </section>
 
       </form>
 
-      <ul class="exist_tasks">
-        <li class="exist_task"
-            v-for="task in tasks"
-        >
-        <div class="exist_task__wrapper">
-            <p class="priority"> {{ PRIORITIES[task.priority] }}</p>
-            <p class="task__name" v-bind:class="{'task__name--done':task.isDone}"> {{ task.name }}</p>
-            <button class="button button--done" v-on:click="task.isDone = !task.isDone"></button>
-            <button class="button button--delete" :id="task.id" v-on:click="deleteTask(task.id)"></button>
-            <button class="button button--expand" v-on:click="task.isExpanded = !task.isExpanded"></button>
-        </div>
-        <p class="task__description" v-if="task.isExpanded">{{ task.description }} </p>
-        </li>
-
+      <ul v-show="getLength" class="existing_tasks">
+        <singleTask
+          v-for="task of tasks" :key="task"
+          v-bind:task="task"
+          v-on:deleteItem="deleteTask(task.id)"
+          />
       </ul>
+
+      <p class="no-task" v-show="!getLength">
+        Please add your first task for today
+      </p>
 
     </main>
   </div>
 </template>
 
-
 <script>
+
+import singleTask from "./taskListItem.vue";
+
   export default {
+  components: {
+    singleTask
+  },
+
     methods: {
 
       addNewTask() {
         let newTask = Object.assign(
           {},
-          this.blankTask,
+          this.blankTask(),
           {
             name: this.newTaskName,
             description: this.newTaskDescription,
@@ -91,8 +88,8 @@
         this.prevTasksList = [];
       },
 
-      deleteTask(some) {
-        this.tasks = this.tasks.filter((task) => Number(task.id) !== some)
+      deleteTask(element) {
+        this.tasks = this.tasks.filter((task) => Number(task.id) !== element)
       }
     },
 
@@ -110,12 +107,6 @@
 
     data() {
       return {
-        PRIORITIES: {
-          1: "High",
-          2: "Middle",
-          3: "Low"
-        },
-
         newTaskName:"",
         newTaskDescription: "",
         newTaskPriority: 3,
@@ -125,65 +116,66 @@
         tasks: [
           {
             id: 1,
-            name: "Buy some bread",
-            description: "You have no food at home.",
+            name: "Fitter happier",
+            description: "More productive Comfortable Not drinking too much",
             isDone: false,
             priority: 1,
-            isExpanded: false
+            isAddjusted: false
           },
           {
             id: 2,
-            name: "Feed your cat",
-            description: "Your cat is hungry.",
+            name: "Regular exercise",
+            description: "Regular exercise at the gym (3 days a week)",
             isDone: false,
             priority: 2,
-            isExpanded: false
+            isAddjusted: false
           },
           {
             id: 3,
-            name: "Go to bed",
-            description: "Your bed is empty. You haven't slept for ages.",
+            name: "Keep in contact",
+            description: "Keep in contact with old friends (enjoy a drink now and then)",
             isDone: false,
             priority: 3,
-            isExpanded: false
+            isAddjusted: false
           },
           {
             id: 4,
-            name: "Take your pills",
-            description: "You are seriously sick. Don't forget to take your meds.",
+            name: "Check credit",
+            description: "Will frequently check credit at (moral) bank (hole in the wall)",
             isDone: true,
             priority: 1,
-            isExpanded: false
+            isAddjusted: false
           },
           {
             id: 5,
-            name: "Call someone",
-            description: "You need help. Call the police",
+            name: "Fitter, healthier and more productive",
+            description: "A pig In a cage On antibiotics",
             isDone: true,
             priority: 1,
-            isExpanded: false
+            isAddjusted: false
           }
         ],
 
-        blankTask: {
-          id: (Math.random(0, 1) * 1000),
-          name: "",
-          description: "",
-          isDone: false,
-          priority: 3,
-          isExpanded: false
+        blankTask:() => {
+          return {
+            id: Math.round(Math.random(0, 1) * 1000),
+            name: "",
+            description: "",
+            isDone: false,
+            priority: 3,
+            isAddjusted: false
+          }
         }
       }
     }
   }
 </script>
 
-
 <style>
   .main {
+    font-family: Arial, Helvetica, sans-serif;
     margin-left: auto;
     margin-right: auto;
-    font-family: Arial, Helvetica, sans-serif;
     width: 420px;
     min-height: 1000px;
     padding-top: 40px;
@@ -193,11 +185,11 @@
     box-sizing: border-box;
   }
 
-  .main__form {
+  .task {
     position: relative;
   }
 
-  .main__task {
+  .task__description {
     box-sizing: border-box;
     width: 330px;
     height: 99px;
@@ -206,16 +198,16 @@
     padding-top: 10px;
   }
 
-  .main__task--name {
+  .task__name {
     height: 30px;
     font-size: 16px;
     line-height: 20px;
     padding-bottom: 10px;
-    color:cadetblue;
+    color: grey;
     margin-bottom: 5px;
   }
 
-  .counter {
+  .task__counter {
     position: absolute;
     top: 10px;
     right: 55px;
@@ -223,22 +215,20 @@
     color: grey;
   }
 
-  .priority-label {
+  .task__priority-label {
     display: inline-block;
-    font-family: Arial, Helvetica, sans-serif;
     font-size: 15px;
     font-weight: 400;
     padding-right: 25px;
-    color: #3078b4;
+    color: tomato;
   }
 
-  .choose_priority {
+  .task__choose_priority {
     display: inline-block;
     width: 250px;
     margin-bottom: 20px;
     margin-top: 0;
   }
-
 
   .buttons {
     display: flex;
@@ -251,7 +241,6 @@
   .button {
     font-size: 12px;
     outline: none;
-    font-family: Arial, Helvetica, sans-serif;
     border-style: solid;
     border-color: rgb(122, 81, 81);
     border-width: 1px;
@@ -297,147 +286,15 @@
     color: ivory;
   }
 
-  .button--delete {
-    height: 20px;
-    width: 20px;
-    position: absolute;
-    bottom: -10px;
-    right: 5px;
-    padding: 0;
-    width: 20px;
-    border-radius: 50%;
-    border: none;
-    background-image: url("./img/del.svg");
-    background-repeat: no-repeat;
-    background-position-x: 5px;
-    background-position-y: 5px;
-    background-color: white;
-  }
-
-  .button--delete:hover {
-    background-color: rgba(0, 0, 0, 0.1);
-  }
-
-  .button--delete:focus {
-    background-color: rgba(0, 0, 0, 0.1);
-  }
-
-  .button--delete:active {
-    background-color: rgba(0, 0, 0, 0.3);
-  }
-
-  .button--done {
-    height: 20px;
-    width: 20px;
-    position: absolute;
-    bottom: -10px;
-    right: 30px;
-    padding: 0;
-    border: none;
-    background-image: url("./img/ok.svg");
-    background-repeat: no-repeat;
-    background-position-x: 5px;
-    background-position-y: 5px;
-    background-color: white;
-    border-radius: 50%;
-  }
-
-  .button--done:hover {
-    background-color: rgba(0, 0, 0, 0.1);
-  }
-
-  .button--done:focus {
-    background-color: rgba(0, 0, 0, 0.1);
-  }
-
-  .button--done:active {
-    background-color: rgba(0, 0, 0, 0.3);
-  }
-
-  .button--expand {
-    height: 15px;
-    width: 15px;
-    position: absolute;
-    bottom: 5px;
-    right: -15px;
-    padding: 0;
-    border: none;
-    background-image: url("./img/expand.svg");
-    background-size: 10px;
-    background-repeat: no-repeat;
-    background-position-x: 2px;
-    background-position-y: 2px;
-    background-color: ghostwhite;
-    border-radius: 2px;
-  }
-
-  .button--expand:hover {
-    background-color: rgba(0, 0, 0, 0.1);
-  }
-
-  .button--expand:focus {
-    background-color: rgba(0, 0, 0, 0.1);
-  }
-
-  .button--expand:active {
-    background-color: rgba(0, 0, 0, 0.3);
-  }
-
-  .exist_tasks {
+  .existing_tasks {
     list-style: none;
     padding: 0;
   }
 
-  .exist_task {
-    display: flex;
-    flex-direction: column;
-    width: 330px;
+  .no-task {
+    color: gainsboro;
+    font-size: 23px;
+    margin-top: 50px;
   }
 
-  .exist_task__wrapper {
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-
-  .task__name {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 12px;
-    line-height: 20px;
-    padding-top: 10px;
-    padding-bottom: 0;
-    padding-left: 10px;
-    vertical-align: middle;
-    width: 266px;
-    height: 30px;
-    background-color:ghostwhite;
-    border-radius: 5px;
-    margin-bottom: 0;
-  }
-
-  .task__name--done {
-    color: green;
-    text-decoration: line-through;
-  }
-
-  .task__description {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 12px;
-    line-height: 20px;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    padding-left: 10px;
-    vertical-align: middle;
-    width: 330px;
-    border: grey solid 1px;
-    border-radius: 5px;
-    margin-top: 0;
-  }
-
-  .priority {
-    font-size: 12px;
-    line-height: 20px;
-    width: 60px;
-  }
 </style>
---
+++ src/taskListItem.vue
@@ -0,0 +1,252 @@
+<template>
+  <li class="existing_tasks"
+    v-on:keydown.esc="task.isAddjusted = false,
+      task.isExpanded = false">
+    <div class="existing_task__wrapper">
+    <p class="existing_task__priority"
+      v-if="!task.isAddjusted"
+      v-bind:class="[
+        {'existing_task__priority--high': task.priority == 1},
+        {'existing_task__priority--medium': task.priority == 2},
+        {'existing_task__priority--low': task.priority == 3}]"
+      v-on:click="task.isAddjusted = true"
+    > {{ PRIORITIES[task.priority] }} </p>
+
+    <select class="existing_task__priority" name="priority" id="priority"
+      v-if="task.isAddjusted"
+      v-on:change="task.priority = $event.target.value"
+      v-bind:class="[
+        {'existing_task__priority--high': task.priority == 1},
+        {'existing_task__priority--medium': task.priority == 2},
+        {'existing_task__priority--low': task.priority == 3}
+      ]">
+
+      <option value="1" v-bind:selected="task.priority == 1">High</option>
+      <option value="2" v-bind:selected="task.priority == 2">Medium</option>
+      <option value="3" v-bind:selected="task.priority == 3">Low</option>
+
+    </select>
+
+      <p class="existing_task__name"
+        v-if="!task.isAddjusted"
+        v-bind:class="{'task__name--done':task.isDone}"
+        v-on:click="task.isAddjusted = true"
+      > {{ task.name }} </p>
+
+      <input class="existing_task__name existing_task__name--adjusted"
+        v-if="task.isAddjusted"
+        v-bind:value="task.name"
+        v-on:input="task.name = $event.target.value"
+        >
+
+      <button class="button button--done"
+        v-on:click="task.isDone = !task.isDone"
+        v-bind:disabled="task.isAddjusted"
+        ></button>
+      <button class="button button--delete" :id="task.id" v-on:click="$emit('deleteItem', task.id)"></button>
+      <button class="button button--expand"
+        v-bind:class="{'button--shrink': task.isAddjusted}"
+        v-on:click="task.isAddjusted = !task.isAddjusted; $emit('closeAll')"></button>
+    </div>
+    <input class="existing_task__description"
+    v-if="task.isAddjusted"
+    v-bind:value="task.description"
+    v-on:input="task.description = $event.target.value"
+    >
+  </li>
+</template>
+
+<script>
+
+  export default {
+    props:['task'],
+
+    data() {
+      return {
+         PRIORITIES: {
+          1: "High",
+          2: "Medium",
+          3: "Low"
+        },
+        newValue: null
+      }
+    },
+
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+  .existing_task {
+    display: flex;
+    flex-direction: column;
+    width: 330px;
+  }
+
+  .existing_task__wrapper {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .existing_task__name {
+    font-size: 12px;
+    line-height: 20px;
+    padding-top: 10px;
+    padding-bottom: 0;
+    padding-left: 10px;
+    vertical-align: middle;
+    width: 266px;
+    height: 30px;
+    background-color:ghostwhite;
+    border-radius: 5px;
+    margin-bottom: 0;
+    border:none;
+  }
+
+  .existing_task__name--adjusted {
+    margin-top: 12px;
+    padding-top: 0;
+    min-height: 40px;
+  }
+
+
+  .task__name--done {
+    color: green;
+    text-decoration: line-through;
+  }
+
+  .existing_task__description {
+    font-size: 12px;
+    line-height: 20px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
+    vertical-align: middle;
+    width: 330px;
+    border: grey solid 1px;
+    border-radius: 5px;
+    margin-top: 0;
+  }
+
+  .existing_task__priority {
+    font-size: 12px;
+    line-height: 20px;
+    width: 60px;
+    font-weight: 600;
+  }
+
+  .existing_task__priority--high {
+    color: tomato;
+  }
+
+  .existing_task__priority--medium {
+    color: orange;
+  }
+
+  .existing_task__priority--low {
+    color: green;
+  }
+
+    .button--delete {
+    height: 20px;
+    width: 20px;
+    position: absolute;
+    bottom: -10px;
+    right: 50px;
+    padding: 0;
+    width: 20px;
+    border-radius: 50%;
+    border: none;
+    background-image: url("./img/del.svg");
+    background-repeat: no-repeat;
+    background-position-x: 5px;
+    background-position-y: 5px;
+    background-color: white;
+  }
+
+  .button--delete:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  .button--delete:focus {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  .button--delete:active {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+
+  .button--done {
+    height: 20px;
+    width: 20px;
+    position: absolute;
+    bottom: -10px;
+    right: 80px;
+    padding: 0;
+    border: none;
+    background-image: url("./img/ok.svg");
+    background-repeat: no-repeat;
+    background-position-x: 5px;
+    background-position-y: 5px;
+    background-color: white;
+    border-radius: 50%;
+  }
+
+  .button--done:disabled {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  .button--done:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  .button--done:focus {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  .button--done:active {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+
+  .button--expand {
+    height: 15px;
+    width: 15px;
+    position: absolute;
+    bottom: 5px;
+    right: 25px;
+    padding: 0;
+    border: none;
+    background-image: url("./img/expand.svg");
+    background-size: 10px;
+    background-repeat: no-repeat;
+    background-position-x: 2px;
+    background-position-y: 2px;
+    background-color: ghostwhite;
+    border-radius: 2px;
+  }
+
+  .button--expand:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  .button--expand:focus {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  .button--expand:active {
+    background-color: rgba(0, 0, 0, 0.3);
+  }
+
+  .button--shrink {
+    background-image: url("./img/shrink.svg");
+    background-position-x: 0;
+    background-position-y: 2px;
+    border: 2px solid indianred;
+  }
+
+</style>>
